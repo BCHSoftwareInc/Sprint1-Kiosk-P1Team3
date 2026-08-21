ad78550be6265bf24df4cb474c3103dab9b4222f
cjwol384 <cjwol384@learner.dvusd.org>
2026-08-21 08:06:24 -0700

    sign in v1
    first code version

---
+++ Signin/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+Console.Write("ATTENDIEE: ");
+string name = Console.ReadLine();
+
+Console.Write("ORG: ");
+string org = Console.ReadLine();
+
+Console.Write("CONTACT: ");
+string contact = Console.ReadLine();
+
+Console.Write("ACCSES: ");
+string accses = Console.ReadLine();
+
+Console.Write("ISSUERE: ");
+string issure = Console.ReadLine();
+
+Console.WriteLine(" +-----------------------------------------------------+");
+Console.WriteLine(" |               APEX ENTERTAINMENT PASS               |");
+Console.WriteLine(" |-----------------------------------------------------|");
+Console.WriteLine($" |   ATTENDIEE:      {name}                            |");
+Console.WriteLine($" |   ORG:            {org}                             |");
+Console.WriteLine($" |   CONTACT:        {contact}                         |");
+Console.WriteLine($" |   ACCSES:         {accses}                          |");
+Console.WriteLine($" |   ISSURE          {issure}                          |");
+Console.WriteLine(" +-----------------------------------------------------+");
